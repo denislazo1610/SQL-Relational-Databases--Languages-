53ff526273004e422d95a0fc56303b9647c1bc96
denislazo1610 <denis1610@byui.edu>
2021-01-30 20:30:02 -0700

    gbfc

---
+++ user.py
@@ -0,0 +1,11 @@
+class user:
+    def __init__(self, username, password, NativeLanguage, LearningLanguage, minutes, joinDate):
+        self.username = username
+        self.password = password
+        self.NativeLanguage = NativeLanguage
+        self.LearningLanguage = LearningLanguage
+        self.minutes = minutes
+        self.joinDate = joinDate
+
+
+
