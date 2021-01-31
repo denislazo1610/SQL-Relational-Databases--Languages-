0e87faa33194e39b8c7d919b791c9a7c4f982f11
denislazo1610 <denis1610@byui.edu>
2021-01-31 01:56:09 -0700

    chnage

--- main.py
+++ main.py
@@ -28,10 +28,16 @@ def addingValues(table, name, password, NL, LL, use, joinDate):
     c.execute('''INSERT INTO {} VALUES (?, ?, ?, ?, ?, ?)'''.format(table), (name, password, NL, LL, use, joinDate))
     conn.commit()
 
+#You are modifying a value in a table
+def modifyValue(table, placeNewValue, newValue, placeCondition, condition):
+    c.execute('''UPDATE {} SET {} = (?) WHERE {} = (?)'''.format(table, placeNewValue, placeCondition), (newValue, condition))
+    conn.commit()
+
 def options():
     print("Choose a option:\n")
     print("\'A\' for adding a new table")
     print("\'B\' for adding a new entity to a table")
+    print("\'C\' for updating a value of a table")
     print("\'Exit\' for finish programm")
 
 
@@ -61,6 +67,22 @@ def action(choice, Tables):
         options()
         choice = input('Enter your choice:')
         action(choice, Tables)
+    
+    elif ((choice == 'c') or (choice == 'C')):
+        print('Updating')
+
+        print("This is all your tables: \n")
+        for x in Tables:
+            print(x)
+        
+        selected = input('Which table do you want to update? ')
+        placeNewValue = input('What attributes do you want to change? ')
+        newValue = input('What do you want to put in there instead? ')
+        placeCondition = input('If there is a condition, in what attribute is your condition? ')
+        condition = input('What is that condition? ')
+
+
+        modifyValue(selected, placeNewValue, newValue, placeCondition, condition)
 
     elif ((choice == 'exit') or (choice == 'Exit')):
         print('Thank you!')
