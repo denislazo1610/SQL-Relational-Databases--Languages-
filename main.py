ea9d1f3134e4e91af46fc4598fd4d7acd8220dc7
denislazo1610 <denis1610@byui.edu>
2021-01-30 22:13:42 -0700

    chnages

--- main.py
+++ main.py
@@ -16,13 +16,16 @@ conn = sqlite3.connect(name)
 c = conn.cursor()
 
 # We are creating a table in the database.
+
 def creatingTable(newTable): 
-    sentence = '''CREATE TABLE ''' + newTable + '''(Username text, Password text, Native_Language text, Learning_language text, minutes_of_use qty, Join_date text)'''
+    sentence ='CREATE TABLE IF NOT EXISTS ' + newTable + ' (Username text, Password text, Native_Language text, Learning_language text, minutes_of_use qty, Join_date text)'
     c.execute(sentence)
+    conn.commit()
+
 
 # We are adding new information to the table. 
 def addingvalues(table, name, password, NL, LL, use, joinDate):
-    c.execute("INSERT INTO ? VALUES (?, ?, ?, ?, ? , ?)", (table, name, password, NL, LL, use, joinDate))
+    c.execute('''INSERT INTO {} VALUES (?, ?, ?, ?, ?, ?)'''.format(table), (name, password, NL, LL, use, joinDate))
     conn.commit()
 
 def options():
@@ -36,12 +39,12 @@ def action(choice, Tables):
     
     if ((choice == 'a') or (choice == 'A')):
         print('Creating a new table')
-        newTable = input("what is your new table:")
+        newTable = input("What is your new table:")
         Tables.append(newTable)
         creatingTable(newTable)
         options()
         choice = input('Enter your choice:')
-        action(choice, Tables)
+        action(choice, Tables) 
 
     elif ((choice == 'b') or (choice == 'B')):
        
@@ -62,17 +65,13 @@ def action(choice, Tables):
     elif ((choice == 'exit') or (choice == 'Exit')):
         print('Thank you!')
 
+
+
 print("Hello, this is a Tracking programm! Which will keep the information of your users.")    
 options()
 print('\n')
 choice = input('Enter your choice:')
 TablesNames = []
 action(choice, TablesNames)
-
-c.execute("INSERT INTO users VALUES ('Josh','PeacetoTheworld','English', 'Spanish', 45 , '2021-01-30')")
-conn.commit()
-
-c.execute("INSERT INTO users VALUES ('Diana','DeathandLove','French', 'Spanish', 60 , '2021-01-30')")
-conn.commit()
 
 conn.close()
